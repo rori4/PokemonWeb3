6fa1734206b4496f161b3d9122722991544f753c
rangel <RangelStoilov@students.softuni.bg>
2018-07-09 13:54:56 +0300

    Transformed to Component & Added Conditional Rendering

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,31 +1,120 @@
 import React from 'react'
-import web3 from '../web3';
+import web3 from '../web3'
+import pokedex from '../pokedex'
 
-const Pokemon = ({ pokemon }) => (
-  <div>
-    <div className="pokemon">
-      <button
-        type="button"
-        className="pokemon__sprite"
-        style={{
-          backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            pokemon.id
-          }.png`})`
-        }}
-      />
-      <p className="pokemon__name">{pokemon.name}</p>
-    </div>
-    <button
-      onClick={() => {buyWithEther()}}
-    >
-      BUY POKEMON
-    </button>
-  </div>
-)
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemonAvailabe: null
+    }
+  }
 
-function buyWithEther() {
-  console.log('Buy POKEMON !');
-  web3.eth.getAccounts().then(console.log);
+  async componentDidMount() {
+    let pokemonAvailable = await pokedex.methods
+      .checkAvailable(this.props.pokemon.id)
+      .call()
+    console.log('pokemonAvailable :', pokemonAvailable)
+    if (pokemonAvailable === true) {
+      this.setState({ pokemonAvailabe: true })
+    } else if (pokemonAvailable === false) {
+      this.setState({ pokemonAvailabe: false })
+    }
+  }
+
+  compo
+
+  renderButton() {
+    let pokemonButton
+    if (this.state.pokemonAvailabe === null) {
+      pokemonButton = <button disabled>Loading...</button>
+    } else if (this.state.pokemonAvailabe === true) {
+      pokemonButton = (
+        <button
+          onClick={() => {
+            buyWithEther(this.props.pokemon.id)
+          }}
+        >
+          BUY POKEMON
+        </button>
+      )
+    } else {
+      pokemonButton = <button disabled>SOLD</button>
+    }
+    return pokemonButton
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="pokemon">
+          <button
+            type="button"
+            className="pokemon__sprite"
+            style={{
+              backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                this.props.pokemon.id
+              }.png`})`
+            }}
+          />
+          <p className="pokemon__name">{this.props.pokemon.name}</p>
+        </div>
+        {this.renderButton()}
+      </div>
+    )
+  }
+}
+
+// const Pokemon = ({ pokemon }) => (
+//   <div>
+//     <div className="pokemon">
+//       <button
+//         type="button"
+//         className="pokemon__sprite"
+//         style={{
+//           backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+//             pokemon.id
+//           }.png`})`
+//         }}
+//       />
+//       <p className="pokemon__name">{pokemon.name}</p>
+//     </div>
+//     {RenderButton(pokemon.id)}
+//   </div>
+// )
+
+// async function RenderButton(pokemonId) {
+//   let pokemonAvailable = await pokedex.methods.checkAvailable(pokemonId).call()
+//   console.log('pokemonAvailable :', pokemonAvailable)
+//   if (pokemonAvailable) {
+//     return (
+//       <button
+//         onClick={() => {
+//           buyWithEther(pokemonId)
+//         }}
+//       >
+//         BUY POKEMON
+//       </button>
+//     )
+//   } else {
+//     return <button disabled>SOLD</button>
+//   }
+// }
+
+async function buyWithEther(pokemonId) {
+  console.log('Buy POKEMON !')
+  console.log('pokemonId :', pokemonId)
+  const accounts = await web3.eth.getAccounts()
+  // let gas = await pokedex.methods.buy(pokemonId).estimateGas({
+  //   from: accounts[0]
+  // });
+  // console.log('gas :', gas);
+  let result = await pokedex.methods.buy(pokemonId).send({
+    from: accounts[0],
+    value: web3.utils.toWei('0.01', 'ether')
+  })
+  // console.log(result);
+  // web3.eth.getAccounts().then(console.log);
 }
 
 export default Pokemon
---
+++ src/pokedex.js
@@ -0,0 +1,60 @@
+import web3 from './web3';
+
+const contractAddress = '0x03a7411614f372a4f78ae3e49de2d2be6584cb71';
+
+const abi = [
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "pokemons",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_pokemonId",
+          "type": "uint256"
+        }
+      ],
+      "name": "buy",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_pokemonId",
+          "type": "uint256"
+        }
+      ],
+      "name": "checkAvailable",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+
+  export default new web3.eth.Contract(abi, contractAddress);
